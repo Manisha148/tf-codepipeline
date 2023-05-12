a8e72ea0881ef90ceefa4ed7be0634d75fd3dc37
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-05-12 17:12:35 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,16 +1,13 @@
 terraform {
-  required_version = "~> 1.2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.5.0"
+      version = "~> 4.0"
     }
   }
 }
 
-# provider block
-
 provider "aws" {
-  #profile = "default"
-  region  = var.aws_region
+ region  = var.aws_region
 }
+
